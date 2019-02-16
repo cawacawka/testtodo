418748ade35a3d0cf6eaf710b7ca3a85d01d148a
cawacawka <alex.under.rip@gmail.com>
2019-02-17 01:20:03 +0700

    single stage 26

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,13 +25,14 @@ def todo(request):
     :return:
     """
     dname = request.param
-    # if "Linux" in platform():
-    #     options = Options()
-    #     options.add_argument("--headless")
-
+    if "Linux" in platform():
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
 
     if dname == "Chrome":
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(chrome_options=chrome_options)
     elif dname == "Firefox":
         browser = webdriver.Firefox()
     browser.delete_all_cookies()
